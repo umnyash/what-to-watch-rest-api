11086e0b6d310be50a3f74a6c2aef6922222afc5
Rustam Sadreev <rustam17.sb@gmail.com>
2026-06-26 23:49:08 +0300

    feat(cli): add entry point

---
+++ src/main.cli.ts
@@ -0,0 +1,33 @@
+import { readdir } from 'node:fs/promises';
+
+import { Command } from './cli/commands/command.interface.js';
+import { CLIApplication } from './cli/index.js';
+
+async function loadCommands(): Promise<Command[]> {
+  const commandsDirUrl = new URL('./cli/commands/', import.meta.url);
+  const commandFileNamePattern = /\.command\.(ts|js)$/;
+  const fileNames = await readdir(commandsDirUrl);
+  const commands: Command[] = [];
+
+  for (const fileName of fileNames) {
+    if (!commandFileNamePattern.test(fileName)) {
+      continue;
+    }
+
+    const moduleUrl = new URL(fileName, commandsDirUrl);
+    const module = await import(moduleUrl.href) as Record<string, new () => Command>;
+    const [CommandClass] = Object.values(module);
+    commands.push(new CommandClass())
+  }
+
+  return commands;
+}
+
+async function bootstrap() {
+  const cliApplication = new CLIApplication();
+  const commands = await loadCommands();
+  cliApplication.registerCommands(commands);
+  await cliApplication.processCommand(process.argv);
+}
+
+await bootstrap();
