a1287554e2f543de94a1c5953f2be7d776a2b465
Rustam Sadreev <rustam17.sb@gmail.com>
2026-06-26 23:56:34 +0300

    feat(file-reader): add CSV parser

---
+++ src/shared/libs/file-reader/csv.parser.ts
@@ -0,0 +1,46 @@
+export class CSVParser {
+  static parseRow(row: string): string[] {
+    const values = []
+    let currentValue = '';
+    let isInsideQuotes = false;
+
+    const isDoubleQuote = (char: string): boolean => char === '"';
+    const isComma = (char: string): boolean => char === ',';
+
+    for (let i = 0; i < row.length; i++) {
+      const currentChar = row[i];
+
+      if (isDoubleQuote(currentChar)) {
+        if (!isInsideQuotes) {
+          isInsideQuotes = true;
+          continue;
+        }
+
+        const nextChar = row[i + 1];
+        const isEscapedQuote = isDoubleQuote(nextChar);
+
+        if (isEscapedQuote) {
+          currentValue += currentChar;
+          i++;
+        } else {
+          isInsideQuotes = false;
+        }
+      } else if (isComma(currentChar) && !isInsideQuotes) {
+        values.push(currentValue);
+        currentValue = '';
+      } else {
+        currentValue += currentChar;
+      }
+    }
+
+    values.push(currentValue);
+    return values;
+  }
+
+  static parse(rawData: string): string[][] {
+    return rawData
+      .split('\n')
+      .filter((row) => row.trim())
+      .map(CSVParser.parseRow.bind(CSVParser));
+  }
+}
