7c1a9b37d3187265cb865dd1ce62f750397f9cdc
Rustam Sadreev <rustam17.sb@gmail.com>
2026-06-26 23:47:24 +0300

    feat(cli): add command manager

---
+++ src/cli/cli.application.ts
@@ -0,0 +1,58 @@
+import { CommandName } from './commands/command-name.enum.js';
+import { Command } from './commands/command.interface.js';
+import { CommandParser } from './command.parser.js';
+
+type CommandCollection = Record<CommandName, Command>;
+
+function isCommandName(name: string): name is CommandName {
+  return Object.values(CommandName).includes(name as CommandName);
+}
+
+export class CLIApplication {
+  private commands: Partial<CommandCollection> = {};
+
+  constructor(
+    private readonly defaultCommandName = CommandName.Help,
+  ) { }
+
+  public registerCommands(commandList: Command[]): void {
+    commandList.forEach((command) => {
+      if (this.commands[command.getName()]) {
+        throw new Error(`Command ${command.getName()} is already registered.`);
+      }
+
+      this.commands[command.getName()] = command;
+    });
+  }
+
+  public async processCommand(argv: string[]): Promise<void> {
+    const parsedCommand = CommandParser.parse(argv);
+    const command = this.getCommand(parsedCommand.name);
+    await command.execute(...parsedCommand.arguments);
+  }
+
+  private getCommand(commandName: string): Command {
+    if (!isCommandName(commandName)) {
+      console.error(`Неизвестная команда: "${commandName}".`);
+      return this.getDefaultCommand();
+    }
+
+    const command = this.commands[commandName];
+
+    if (!command) {
+      throw new Error(`Command "${commandName}" is not registered.`);
+    }
+
+    return command;
+  }
+
+  private getDefaultCommand(): Command {
+    const command = this.commands[this.defaultCommandName];
+
+    if (!command) {
+      throw new Error(`Default command ("${this.defaultCommandName}") is not registered.`);
+    }
+
+    return command;
+  }
+}
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,1 +1,2 @@
+export { CLIApplication } from './cli.application.js';
 export { Command } from './commands/command.interface.js';
