8be5d53e3794c77322fa762dbbac1ff0a3d88c07
Rustam Sadreev <rustam17.sb@gmail.com>
2026-07-12 22:37:31 +0300

    feat(logger): define logger interface

---
+++ src/shared/libs/logger/logger.interface.ts
@@ -0,0 +1,6 @@
+export interface Logger {
+  debug(message: string, ...args: unknown[]): void;
+  info(message: string, ...args: unknown[]): void;
+  warn(message: string, ...args: unknown[]): void;
+  error(error: Error, message: string, ...args: unknown[]): void;
+}
