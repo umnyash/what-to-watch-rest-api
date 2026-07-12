22660e5c79cef5d9c581cc715135db16ff0d4478
Rustam Sadreev <rustam17.sb@gmail.com>
2026-07-13 00:04:28 +0300

    feat(rest): integrate config

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,10 +1,12 @@
 import { PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
+import { RestConfig } from './shared/libs/config/index.js';
 
 async function bootstrap() {
   const logger = new PinoLogger();
+  const config = new RestConfig(process.env);
 
-  const application = new RestApplication(logger);
+  const application = new RestApplication(config, logger);
   await application.init();
 }
 
--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,11 +1,14 @@
+import { Config } from '../shared/libs/config/index.js';
 import { Logger } from '../shared/libs/logger/index.js';
 
 export class RestApplication {
   constructor(
+    private readonly config: Config,
     private readonly logger: Logger,
   ) { }
 
   public async init() {
     this.logger.info('Initializing application…');
+    this.logger.info(`PORT environment variable: ${String(this.config.get('PORT'))}`);
   }
 }
