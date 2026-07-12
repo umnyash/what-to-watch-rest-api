a426be6646d1b916a547a8b55b7d2fe6066b0139
Rustam Sadreev <rustam17.sb@gmail.com>
2026-07-12 23:35:31 +0300

    feat(logger): configure file logging

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -1,9 +1,26 @@
-import { pino } from 'pino';
+import { pino, transport } from 'pino';
+import { resolve } from 'node:path';
 
 import { Logger } from './logger.interface.js';
+import { getCurrentModuleDirectoryPath } from '../../utils/index.js';
 
 export class PinoLogger implements Logger {
-  private readonly logger = pino({ level: 'debug' });
+  private readonly logger = pino({ level: 'debug' }, transport({
+    targets: [
+      {
+        target: 'pino/file',
+        level: 'debug',
+        options: {
+          destination: resolve(getCurrentModuleDirectoryPath(), '../../../logs/rest.log'),
+        },
+      },
+      {
+        target: 'pino/file',
+        level: 'info',
+        options: {},
+      },
+    ],
+  }));
 
   public debug(message: string, ...args: unknown[]): void {
     this.logger.debug(message, ...args);
