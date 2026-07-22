b26af610fcb66ea9e55598d7514d520da80ef749
Rustam Sadreev <rustam17.sb@gmail.com>
2026-07-22 18:37:06 +0300

    types(shared): define genre type

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,3 +1,4 @@
 export { Component } from './component.enum.js';
+export { Genre } from './genre.type.js';
 export { MockServerData } from './mock-server-data.type.js';
 export { Movie } from './movie.type.js';
